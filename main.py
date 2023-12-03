bd803500b8518bf9e9cc9af538113ceca2420b61
Mike <123130334+collinhuqchoat@users.noreply.github.com>
2023-12-03 16:23:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import time
+import pyttsx3
 
+def say_phrase():
+    engine = pyttsx3.init()
+
+    phrase = "yor phrase"
+
+    engine.say(phrase)
+    engine.runAndWait()
+
+if __name__ == "__main__":
+    interval_seconds = 6000
+
+    while True:
+        time.sleep(interval_seconds)
+        say_phrase()
